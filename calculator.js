9a11586ea2a0694438cf3efb6360c578014e791a
Hilton Au <hilton.h.au@gmail.com>
2021-11-06 14:35:02 -0400

    Changed calculator JS to improve calculator functionality. Also resolved bug where double-clicking operators will zero out the number.

--- calculator.js
+++ calculator.js
@@ -1,6 +1,7 @@
 let screenValue = document.querySelector("#screen-value");
-let currentValue = '';
+let currentValue = '0';
 let equation = 0;
+let currentOperator = '';
 let previousOperator = '';
 let allButtons = document.querySelectorAll('button');
 let numbers = document.querySelectorAll('.numbers');
@@ -13,103 +14,117 @@ let decimal = document.querySelector('#decimal');
 
 
 let updateScreen = function() {
-    screenValue.textContent = currentValue;
+    screenValue.textContent = currentValue
 };
 
 
 updateScreen();
 
+
 numbers.forEach((number) => {
     number.addEventListener('click',() => {
-        if (currentValue.length < 13){
-        currentValue = currentValue.concat(number.innerText);
-        updateScreen();
-        };
-    });
+        previousOperator = currentOperator
+        if (currentValue.length >= 15){
+            alert(`Number exceeds screensize!`)
+        }
+        else if (currentValue.length < 15 && currentValue != '0'){
+            currentValue = currentValue.concat(number.innerText)
+            updateScreen()
+        }
+        else {
+            currentValue = number.innerText
+            updateScreen()
+        }
+    })
 });
 
+
 decimal.addEventListener('click',() => {
-    if (currentValue.length < 13){
+    if (currentValue.length < 15){
         if (currentValue.includes('.')) {
 
         }
         else {
-            currentValue = currentValue.concat(decimal.innerText);
-            updateScreen();
-        };
-    };
+            currentValue = currentValue.concat(decimal.innerText)
+            updateScreen()
+        }
+    }
+    else{
+        alert(`Number exceeds screensize!`)
+    }
 });
 
 
 backspace.addEventListener('click',()=> {
-    currentValue = currentValue.slice(0,-1);
-    updateScreen();
+    currentValue = currentValue.substring(0,currentValue.length-1)
+    if (currentValue == ""){
+        currentValue = "0"
+    }
+    updateScreen()
 });
 
 clear.addEventListener('click',()=> {
-    equation = 0;
-    currentValue = '';
-    previousOperator = '';
-    updateScreen();
-})
+    equation = 0
+    currentValue = '0'
+    previousOperator = ''
+    updateScreen()
+});
+
+
+// There is a bug where if operator is doubleclicked, then equation is zeroed out
 
 operators.forEach((operator) => {
     operator.addEventListener('click',() => {
-        switch(previousOperator){
-            case '+':
-                equation += Number(currentValue);
-                screenValue.textContent = equation;
-                currentValue = '';
-                previousOperator = operator.innerText;
-            break;
-            case '-':
-                equation -= Number(currentValue);
-                screenValue.textContent = equation;
-                currentValue = '';
-                previousOperator = operator.innerText;
-            break;
-            case 'x':
-                equation *= Number(currentValue);
-                screenValue.textContent = equation;
-                currentValue = '';
-                previousOperator = operator.innerText;
-            break;
-            case '/':
-                equation /= Number(currentValue);
-                screenValue.textContent = equation;
-                currentValue = '';
-                previousOperator = operator.innerText;
-            break;
-            case '=':
-                screenValue.textContent = equation;
-                currentValue = '';
-                previousOperator = '';
-                equation = 0;
-            break;
-            default:
-                previousOperator = operator.innerText;
-                equation = Number(currentValue);
-                screenValue.textContent = equation;
-                currentValue = '';
+        if (currentOperator == previousOperator) {
+            switch(previousOperator){
+                case '+':
+                        equation += Number(currentValue)
+                        screenValue.textContent = String(equation).substring(0,15)
+                break
+                case '-':
+                    equation -= Number(currentValue)
+                    screenValue.textContent = String(equation).substring(0,15)
+                break
+                case 'x':
+                    equation *= Number(currentValue)
+                    screenValue.textContent = String(equation).substring(0,15)
+
+                break
+                case '/':
+                    equation /= Number(currentValue)
+                    screenValue.textContent = String(equation).substring(0,15)
+                break
+                default:
+                        equation = Number(currentValue)
+                        screenValue.textContent = String(equation).substring(0,15)
+            }
+            currentValue = ''
+            currentOperator = operator.innerText
         }
-    });
+    })
 });
 
 
 percent.addEventListener('click',() => {
-    let percentage = (Number(currentValue)/100);
-    currentValue = percentage.toString();
-    updateScreen();
+    if (previousOperator == 'x'){
+        let percentage = (Number(currentValue)/100)
+        currentValue = percentage.toString()
+        updateScreen()
+    }
 });
 
 posNeg.addEventListener('click',() => {
-    if((Number(currentValue)) < 0){
-        currentValue.slice(0,1);
-        updateScreen();
+    let number = Number(currentValue)
+    if (number == 0){
+        updateScreen()
+    }
+    else if (number < 0){
+        currentValue = currentValue.substring(1)
+        updateScreen()
     }
     else{
-        currentValue = '-' + currentValue;
-        updateScreen();
+        currentValue = '-' + currentValue
+        updateScreen()
     }
 
 });
